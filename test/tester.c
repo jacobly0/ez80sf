0e1a945cceb925c137398ca7c97e950f44d9938f
Jacob Young <jacobly0@users.noreply.github.com>
2018-01-14 13:50:00 -0500

    Debug stack state.

--- test/tester.c
+++ test/tester.c
@@ -10,6 +10,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 void gui_do_stuff(void) {}
@@ -30,7 +31,7 @@ static const uint32_t stack = 0xD40000;
 static const uint32_t retaddr = 0x3FFFFE;
 static const uint8_t halt = 0166;
 
-typedef struct pair8_24 { uint32_t x : 24; uint8_t y : 8; } pair8_24_t;
+typedef struct pair8_24 { uint32_t x : 24, y : 8; } pair8_24_t;
 #define bitcast(dst, src, ...) ((union { src __src; dst __dst; }){ __VA_ARGS__ }.__dst)
 bool same(float x, float y) {
   return !((bitcast(uint32_t, float, x) ^ bitcast(uint32_t, float, y)) &
@@ -41,6 +42,18 @@ static bool check(eZ80registers_t in, eZ80registers_t out, const char **reason) 
   (void)in;
   if (!cpu.halted) {
     *reason = "Halt never reached";
+    return false;
+  }
+  if (!cpu.ADL) {
+    *reason = "In Z80 mode";
+    return false;
+  }
+  if (cpu.MADL) {
+    *reason = "MADL set";
+    return false;
+  }
+  if (out.MBASE != 0xD0) {
+    *reason = "MBASE changed";
     return false;
   }
   if (out.PC != retaddr + 1) {
@@ -78,30 +91,39 @@ static uint32_t random_reg(void) {
     random_edge(value >> 28 & 0xF, value >> 8 & 0xFF) << 8 |
     random_edge(value >> 24 & 0xF, value >> 0 & 0xFF);
 }
-static void print_regs(eZ80registers_t *regs) {
+static void print_regs(eZ80registers_t *regs, uint8_t stack[8][3]) {
   fprintf(stderr,
-	  "\tAF %04X     %04X AF'\n"
-	  "\tBC %06X %06X BC'\n"
-	  "\tDE %06X %06X DE'\n"
-	  "\tHL %06X %06X HL'\n"
-	  "\tIX %06X %06X SP\n"
-	  "\tIY %06X %06X PC\n"
-	  "\tABC %e\n"
-	  "\tEHL %e\n",
+	  "\tAF %04X     %04X AF' (SP+00) %06X\n"
+	  "\tBC %06X %06X BC' (SP+03) %06X\n"
+	  "\tDE %06X %06X DE' (SP+06) %06X\n"
+	  "\tHL %06X %06X HL' (SP+09) %06X\n"
+	  "\tIX %06X %06X SP  (SP+12) %06X\n"
+	  "\tIY %06X %06X PC  (SP+15) %06X\n"
+	  "\tABC %13e    (SP+18) %06X\n"
+	  "\tEHL %13e    (SP+21) %06X\n",
 	  regs->AF, regs->_AF,
+	  stack ? stack[0][2] << 16 | stack[0][1] << 8 | stack[0][0] : retaddr,
 	  regs->BC, regs->_BC,
+	  stack ? stack[1][2] << 16 | stack[1][1] << 8 | stack[1][0] : 0,
 	  regs->DE, regs->_DE,
+	  stack ? stack[2][2] << 16 | stack[2][1] << 8 | stack[2][0] : 0,
 	  regs->HL, regs->_HL,
+	  stack ? stack[3][2] << 16 | stack[3][1] << 8 | stack[3][0] : 0,
 	  regs->IX, regs->SPL,
+	  stack ? stack[4][2] << 16 | stack[4][1] << 8 | stack[4][0] : 0,
 	  regs->IY, regs->PC,
+	  stack ? stack[5][2] << 16 | stack[5][1] << 8 | stack[5][0] : 0,
 	  bitcast(float, pair8_24_t, { regs->BC, regs->A }),
-	  bitcast(float, pair8_24_t, { regs->HL, regs->E }));
+	  stack ? stack[6][2] << 16 | stack[6][1] << 8 | stack[6][0] : 0,
+	  bitcast(float, pair8_24_t, { regs->HL, regs->E }),
+	  stack ? stack[7][2] << 16 | stack[7][1] << 8 | stack[7][0] : 0);
 }
 
 int main(int argc, char **argv) {
   if (argc != 3) return 2;
   uint64_t failures = 0, iterations = 10, firstFailure;
   eZ80registers_t firstIn, firstOut;
+  uint8_t firstStack[8][3];
   const char *firstReason;
   emu_load(argv[1], NULL);
   flash.waitStates = 4;
@@ -110,6 +132,7 @@ int main(int argc, char **argv) {
   mem_poke_byte(stack - 1, (uint8_t)(retaddr >> 16));
   mem_poke_byte(stack - 2, (uint8_t)(retaddr >>  8));
   mem_poke_byte(stack - 3, (uint8_t)(retaddr >>  0));
+  cpu.registers.MBASE = 0xD0;
   for (uint64_t i = 0; i != iterations; i++) {
     cpu.registers.AF = random_reg();
     cpu.registers._AF = random_reg();
@@ -131,11 +154,14 @@ int main(int argc, char **argv) {
     cpu_flush(entry, 1);
     cpu_execute();
     eZ80registers_t out = cpu.registers;
+    uint8_t outStack[8][3];
+    virt_mem_cpy(&outStack, out.SPL, sizeof outStack);
     const char *reason;
     if (!check(in, out, &reason) && failures++ == 0) {
       firstFailure = i;
       firstIn = in;
       firstOut = out;
+      memcpy(&firstStack, &outStack, sizeof outStack);
       firstReason = reason;
     }
   }
@@ -145,9 +171,9 @@ int main(int argc, char **argv) {
 	  iterations, 1.0 / iterations * (cpu_total_cycles() - cpu.haltCycles) - 4);
   if (failures) {
     fprintf(stderr, "%s for test #%" PRIu64 " with input:\n", firstReason, firstFailure);
-    print_regs(&firstIn);
+    print_regs(&firstIn, NULL);
     fprintf(stderr, "and output:\n");
-    print_regs(&firstOut);
+    print_regs(&firstOut, firstStack);
     fprintf(stderr, "%08" PRIX32 "\n", bitcast(uint32_t, float, bitcast(float, pair8_24_t, { firstIn.BC, firstIn.A }) + bitcast(float, pair8_24_t, { firstIn.HL, firstIn.E })));
   }
   asic_free();
