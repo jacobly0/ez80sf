c774ad0ba9d7c365a8307e8990867bc02636e260
Jacob Young <jacobly0@users.noreply.github.com>
2022-03-08 03:24:07 -0500

    Test toolchain routines.

--- Makefile
+++ Makefile
@@ -6,32 +6,35 @@ CHECK = *
 OFFSET = 0
 ITERATIONS = 1000000
 MAX_CYCLES = 15000
+FASMG_VERSION = jje9
+
+rwildcard = $(strip $(foreach d,$(wildcard $1/*),$(call rwildcard,$d,$2) $(filter $(subst %%,%,%$(subst *,%,$2)),$d)))
 
 ez80sf.rom: external/fasmg ez80sf.src
 	@git submodule update --init --recursive -- external/fasmg-ez80
 	$^ $@
-ez80sf.src: src/*.inc
+ez80sf.src: $(sort $(call rwildcard,src,*.inc))
 	@{ \
 		for file in $(addprefix external/fasmg-ez80/,ez80.inc symbol_table.inc) $^; do \
 			printf "include '$$file'\n"; \
 		done; \
 	} >$@
 
-external/fasmg: external/fasmg.zip
-	unzip -j -o $< -d $(@D) `test "\`uname -s\`" = Darwin && printf source/macos/`$(@F)
+external/fasmg: external/fasmg.$(FASMG_VERSION).zip
+	unzip -j -o $< -d $(@D) `test "\`uname -s\`" = Darwin && printf source/macos/x64/`$(@F)
 	chmod +x $@
-external/fasmg.zip:
-	wget https://flatassembler.net/fasmg.zip --output-document=$@
+external/fasmg.$(FASMG_VERSION).zip:
+	wget https://flatassembler.net/fasmg.$(FASMG_VERSION).zip --output-document=$@
 
 check: ez80sf.rom
 	@git submodule update --init --recursive -- external/CEmu
 	$(MAKE) -C external/CEmu/core CC="$(CC)" CXX="$(CXX)" CFLAGS="$(CFLAGS)" libcemucore.a
-	@grep -h "^?\?\w\+: *; *CHECK: *" $(patsubst %,src/%.inc,$(CHECK)) | { \
-		exit=0; while read check; do \
-			$(CC) $(CFLAGS) $(LDFLAGS) -I external/CEmu/core -DCHECK="$${check##*:}" -DOFFSET="$(OFFSET)" -DITERATIONS="$(ITERATIONS)" \
+	@sed -E -e '/^\??([[:alnum:]_.]+) *: *;.*CHECK:/!d;s/^\??([[:alnum:]_.]+) *: *; *(PREREQ:(.*))?CHECK:(.*)$$/\1\t\3\t\4/;s/\t\t/\ttrue\t/' $(patsubst %,src/%.inc,$(CHECK)) | { \
+		exit=0; IFS=\	; while read label prereq check; do \
+			$(CC) $(CFLAGS) $(LDFLAGS) -I external/CEmu/core -DPREREQ="$$prereq" -DCHECK="$${check#*CHECK:}" -DOFFSET="$(OFFSET)" -DITERATIONS="$(ITERATIONS)" \
 				-DMAX_CYCLES="$(MAX_CYCLES)" test/tester.c external/CEmu/core/libcemucore.a -lm -o test/tester && \
-			printf '%s' "Testing $${check%%:*}..." && \
-			test/tester $^ "`grep "^$${check%%:*} *= *" ez80sf.lab | cut -d= -f2`" || \
+			printf '%s' "Testing $$label..." >&2 && \
+			test/tester $^ "`grep "^$$label *= *" ez80sf.lab | cut -d= -f2`" || \
 			exit=1; \
 		done; exit $$exit; \
 	}
@@ -43,5 +46,5 @@ distclean:
 	git submodule deinit --all --force
 	rm -rf ez80sf.* test/tester external/fasmg
 
-.INTERMEDIATE: ez80sf.src external/fasmg.zip
+.INTERMEDIATE: ez80sf.src external/fasmg.$(FASMG_VERSION).zip
 .PHONY: check clean distclean
--- test/tester.c
+++ test/tester.c
@@ -31,12 +31,20 @@ static const uint32_t retaddr = 0x3FFFFE;
 static const uint8_t halt = 0166;
 
 typedef struct pair8_24 { uint32_t x : 24, y : 8; } pair8_24_t;
+typedef struct tuple16_24_24 { uint64_t x : 24, y : 24, z : 16; } tuple16_24_24_t;
 #define bitcast(dst, src, ...) ((union { src __src; dst __dst; }){ __VA_ARGS__ }.__dst)
 bool same(float x, float y) {
   return x == y || (isnan(x) && isnan(y));
 }
 
-static bool check(eZ80registers_t in, eZ80registers_t out, const char **reason) {
+static bool prereq(eZ80registers_t in, uint8_t inStack[10][3]) {
+  (void)in;
+  (void)inStack;
+  return (PREREQ);
+}
+
+static bool check(eZ80registers_t in, uint8_t inStack[10][3], eZ80registers_t out, uint8_t outStack[10][3], const char **reason) {
+  (void)in, (void)inStack, (void)out, (void)outStack;
   if (!cpu.halted) {
     *reason = "Halt never reached";
     return false;
@@ -55,10 +63,6 @@ static bool check(eZ80registers_t in, eZ80registers_t out, const char **reason) 
   }
   if (out.PC != retaddr + 1) {
     *reason = "Incorrect return address";
-    return false;
-  }
-  if (mem_peek_long(stack - 3) != retaddr) {
-    *reason = "Corrupted return address";
     return false;
   }
   if (out.SPL != in.SPL + 3) {
@@ -90,8 +94,9 @@ static uint8_t random_edge(uint8_t type, uint8_t value) {
   static const uint8_t edges[] = { 0x00, 0x00, 0x01, 0x3F, 0x40, 0x7F, 0x7F, 0x80, 0x80, 0xBF, 0xC0, 0xFE, 0xFF, 0xFF };
   return type < sizeof(edges) ? edges[type] : value;
 }
-static uint32_t random_reg(void) {
+static uint32_t random_reg(uint8_t type) {
   uint64_t value = random64();
+  if (type & 1) return value >> 40;
   return random_edge(value >> 60 & 15, value >> 52) << 16 |
          random_edge(value >> 48 & 15, value >> 40) << 8 |
          random_edge(value >> 36 & 15, value >> 28);
@@ -109,59 +114,80 @@ static void print_regs(eZ80registers_t *regs, uint8_t stack[10][3]) {
           "\tABC %-+16.6a (SP+24) %06X\n"
           "\tEHL %-+16.6a (SP+27) %06X\n",
           regs->AF, regs->_AF,
-          stack ? stack[0][2] << 16 | stack[0][1] << 8 | stack[0][0] : retaddr,
+          stack[0][2] << 16 | stack[0][1] << 8 | stack[0][0],
           regs->BC, regs->_BC,
-          stack ? stack[1][2] << 16 | stack[1][1] << 8 | stack[1][0] : 0,
+          stack[1][2] << 16 | stack[1][1] << 8 | stack[1][0],
           regs->DE, regs->_DE,
-          stack ? stack[2][2] << 16 | stack[2][1] << 8 | stack[2][0] : 0,
+          stack[2][2] << 16 | stack[2][1] << 8 | stack[2][0],
           regs->HL, regs->_HL,
-          stack ? stack[3][2] << 16 | stack[3][1] << 8 | stack[3][0] : 0,
+          stack[3][2] << 16 | stack[3][1] << 8 | stack[3][0],
           regs->IX, regs->SPL,
-          stack ? stack[4][2] << 16 | stack[4][1] << 8 | stack[4][0] : 0,
+          stack[4][2] << 16 | stack[4][1] << 8 | stack[4][0],
           regs->IY, regs->PC,
-          stack ? stack[5][2] << 16 | stack[5][1] << 8 | stack[5][0] : 0,
+          stack[5][2] << 16 | stack[5][1] << 8 | stack[5][0],
           bitcast(float, pair8_24_t, { regs->BC, regs->A }),
-          stack ? stack[6][2] << 16 | stack[6][1] << 8 | stack[6][0] : 0,
+          stack[6][2] << 16 | stack[6][1] << 8 | stack[6][0],
           bitcast(float, pair8_24_t, { regs->HL, regs->E }),
-          stack ? stack[7][2] << 16 | stack[7][1] << 8 | stack[7][0] : 0,
+          stack[7][2] << 16 | stack[7][1] << 8 | stack[7][0],
           bitcast(float, pair8_24_t, { regs->BC, regs->A }),
-          stack ? stack[8][2] << 16 | stack[8][1] << 8 | stack[8][0] : 0,
+          stack[8][2] << 16 | stack[8][1] << 8 | stack[8][0],
           bitcast(float, pair8_24_t, { regs->HL, regs->E }),
-          stack ? stack[9][2] << 16 | stack[9][1] << 8 | stack[9][0] : 0);
+          stack[9][2] << 16 | stack[9][1] << 8 | stack[9][0]);
+}
+
+static void hint(eZ80registers_t in, uint8_t inStack[10][3], eZ80registers_t out, uint8_t outStack[10][3]) {
+  (void)in, (void)inStack, (void)out, (void)outStack;
+  //fprintf(stderr, "%016lX\n", bitcast(uint64_t, tuple16_24_24_t, { out.HL, out.DE, out.BCS }));
+  //fprintf(stderr, "%08X %-+15.8e %-+16.6a\n", bitcast(uint32_t, float, x), x, x);
+  //fprintf(stderr, "%g / %g -> %g != %g;\n", in.HL * 0x1p-12, in.DE * 0x1p-12, out.HL * 0x1p-12, ((((uint64_t)in.HL << 13) / in.DE + 0) >> 1) * 0x1p-12);
+  //fprintf(stderr, "%.8g / %.8g -> %.8g != %.8g;\n", ((int32_t)((uint32_t)in.HL << 8) >> 8) * 0x1p-12, ((int32_t)((uint32_t)in.DE << 8) >> 8) * 0x1p-12, ((int32_t)((uint32_t)out.HL << 8) >> 8) * 0x1p-12, (((int32_t)((uint32_t)in.HL << 8) >> 8) * 0x1p-12) / (((int32_t)((uint32_t)in.DE << 8) >> 8) * 0x1p-12));
+  //fprintf(stderr, "%016lX -> %016lX\n", bitcast(uint64_t, tuple16_24_24_t, { in.HL, in.DE, in.BCS }), bitcast(uint64_t, tuple16_24_24_t, { out.HL, out.DE, out.BCS }));
+  //fprintf(stderr, "%016lX -> %016lX != %016lX\n", *(int64_t*)inStack[1], bitcast(uint64_t, tuple16_24_24_t, { out.HL, out.DE, out.BCS }), labs(*(int64_t*)inStack[1]));
+  //fprintf(stderr, "%016lX %% %016lX -> %016lX != %016lX\n", bitcast(int64_t, tuple16_24_24_t, { in.HL, in.DE, in.BCS }), *(int64_t*)inStack[1], bitcast(uint64_t, tuple16_24_24_t, { out.HL, out.DE, out.BCS }), bitcast(int64_t, tuple16_24_24_t, { in.HL, in.DE, in.BCS }) % *(int64_t*)inStack[1]);
 }
 
 #define UINT64_C_(x) UINT64_C(x)
 
 int main(int argc, char **argv) {
   if (argc != 3) return 2;
-  const uint64_t iterations = UINT64_C_(ITERATIONS);
-  uint64_t failures = 0, firstFailure, minCycles = ~UINT64_C(0), maxCycles = UINT64_C(0);
+  uint64_t valid = 0, failures = 0, firstFailure, minCycles = ~UINT64_C(0), maxCycles = UINT64_C(0);
   eZ80registers_t firstIn, firstOut;
-  uint8_t firstStack[10][3];
   const char *firstReason;
   emu_load(false, argv[1]);
+  cpu.cycles = 0;
   flash.waitStates = 4;
   mem_poke_byte(retaddr, halt);
   int entry = atoi(argv[2]);
-  mem_poke_byte(stack - 1, (uint8_t)(retaddr >> 16));
-  mem_poke_byte(stack - 2, (uint8_t)(retaddr >>  8));
-  mem_poke_byte(stack - 3, (uint8_t)(retaddr >>  0));
+  uint8_t inStack[10][3], firstInStack[10][3], firstOutStack[10][3];
+  inStack[0][0] = (uint8_t)(retaddr >>  0);
+  inStack[0][1] = (uint8_t)(retaddr >>  8);
+  inStack[0][2] = (uint8_t)(retaddr >> 16);
   cpu.registers.MBASE = 0xD0;
-  for (uint64_t i = 0; i != UINT64_C_(OFFSET) * 10; i++)
+  for (uint64_t i = 0; i != UINT64_C_(OFFSET) * 13; i++)
     random64();
-  for (uint64_t i = 0; i != iterations; i++) {
+  for (uint64_t i = UINT64_C_(OFFSET); i != UINT64_C_(ITERATIONS); i++) {
     uint64_t lastCycles = sched_total_cycles() - cpu.haltCycles;
-    cpu.registers.AF = random_reg();
-    cpu.registers._AF = random_reg();
-    cpu.registers.BC = random_reg();
-    cpu.registers._BC = random_reg();
-    cpu.registers.DE = random_reg();
-    cpu.registers._DE = random_reg();
-    cpu.registers.HL = random_reg();
-    cpu.registers._HL = random_reg();
-    cpu.registers.IX = random_reg();
-    cpu.registers.IY = random_reg();
+    cpu.registers.AF = random_reg(i);
+    cpu.registers._AF = random_reg(i);
+    cpu.registers.BC = random_reg(i);
+    cpu.registers._BC = random_reg(i);
+    cpu.registers.DE = random_reg(i);
+    cpu.registers._DE = random_reg(i);
+    cpu.registers.HL = random_reg(i);
+    cpu.registers._HL = random_reg(i);
+    cpu.registers.IX = random_reg(i);
+    cpu.registers.IY = random_reg(i);
+    for (int index = 1; index <= 3; ++index) {
+        uint32_t value = random_reg(i);
+        inStack[index][0] = (uint8_t)(value >>  0);
+        inStack[index][1] = (uint8_t)(value >>  8);
+        inStack[index][2] = (uint8_t)(value >> 16);
+    }
     cpu.registers.SPL = stack - 3;
+    memcpy(phys_mem_ptr(cpu.registers.SPL, sizeof inStack), inStack, sizeof inStack);
+    if (!prereq(cpu.registers, inStack))
+        continue;
+    ++valid;
     cpu.inBlock = cpu.halted = false;
     cpu.baseCycles += cpu.cycles;
     cpu.cycles = 0;
@@ -174,11 +200,12 @@ int main(int argc, char **argv) {
     uint8_t outStack[10][3];
     virt_mem_cpy(&outStack, out.SPL, sizeof outStack);
     const char *reason;
-    if (!check(in, out, &reason) && failures++ == 0) {
+    if (!check(in, inStack, out, outStack, &reason) && failures++ == 0) {
       firstFailure = i;
       firstIn = in;
       firstOut = out;
-      memcpy(&firstStack, &outStack, sizeof outStack);
+      memcpy(firstInStack, inStack, sizeof inStack);
+      memcpy(firstOutStack, outStack, sizeof outStack);
       firstReason = reason;
     }
     uint64_t curCycles = sched_total_cycles() - cpu.haltCycles - lastCycles;
@@ -186,13 +213,16 @@ int main(int argc, char **argv) {
     if (maxCycles < curCycles) maxCycles = curCycles;
   }
   fprintf(stderr, " \33[%dm%.6f%% failed\33[m in \33[33m%" PRIu64 "/%f/%" PRIu64 " cycles\33[m minimum/average/maximum\n",
-          failures ? 31 : 32, 100.0 / iterations * failures,
-          minCycles, 1.0 / iterations * (sched_total_cycles() - cpu.haltCycles), maxCycles);
+          failures ? 31 : 32, 100.0 * failures / valid,
+          minCycles, 1.0 / valid * (sched_total_cycles() - cpu.haltCycles), maxCycles);
   if (failures) {
     fprintf(stderr, "%s for test #%" PRIu64 " with input:\n", firstReason, firstFailure);
-    print_regs(&firstIn, NULL);
+    print_regs(&firstIn, firstInStack);
     fprintf(stderr, "and output:\n");
-    print_regs(&firstOut, firstStack);
+    print_regs(&firstOut, firstOutStack);
+    //hint(bitcast(float, pair8_24_t, { firstIn.BC, firstIn.A }) * bitcast(float, pair8_24_t, { firstIn.HL, firstIn.E }));
+    //hint(bitcast(float, pair8_24_t, { firstIn.BC, firstIn.A }) / bitcast(float, pair8_24_t, { firstIn.HL, firstIn.E }));
+    hint(firstIn, firstInStack, firstOut, firstOutStack);
   }
   asic_free();
   return failures != 0;
